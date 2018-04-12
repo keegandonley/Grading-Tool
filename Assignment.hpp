511a991fde42458989a0fdcfa1b4f26c6e33e54c
Keegan Donley <keegan.donley@gmail.com>
2018-04-11 23:54:28 -0700

    Added documentation to Assignment class

--- Assignment.hpp
+++ Assignment.hpp
@@ -1,3 +1,22 @@
+/**
+ * File: Assignment.hpp
+ * Author: Keegan Donley
+ * Date Updated: 4/11/18
+ * ----------------------------------------
+ * Author contact: kd@keegandonley.com
+ * Author website: https://www.keegandonley.com
+ *
+ * This file contains the Assignment class.
+ * This class handles running the program and collecting feedback for each
+ * student. The Assignment should be initialized with a metadata set that has
+ * been already generated.
+ *
+ * Private data members:
+ * 	aMeta * assignmentData - the assignment metadata as defined by aMeta.
+ * 	vector<Student *> students - a vector containing all the students and their
+ * 					data for the current grading session.
+ **/
+
 #include <string>
 #include <vector>
 
@@ -20,7 +39,6 @@ public:
 private:
     aMeta * assignmentData;
     std::vector<Student *> students;
-
-
 };
+
 #endif
